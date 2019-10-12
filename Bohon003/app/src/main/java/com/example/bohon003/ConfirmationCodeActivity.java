26e8ff22dcef717c6f425d0d1b1fac7109feaf16
Avishak Chakroborty <aurgho1998@gmail.com>
2019-10-13 04:39:35 +0600

    Add files via upload

---
+++ Bohon003/app/src/main/java/com/example/bohon003/ConfirmationCodeActivity.java
@@ -0,0 +1,19 @@
+package com.example.bohon003;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class ConfirmationCodeActivity extends AppCompatActivity {
+
+    private Button confirmationButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirmation_code);
+
+
+    }
+}
