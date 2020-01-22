17f4b20087b433008130f624dbf5f117bd9e4e06
jagonmoy <43012506+jagonmoy@users.noreply.github.com>
2020-01-22 07:38:52 +0600

    Create activity_Main.java

---
+++ BohonFin001/jagonmoy_database(new)/activity_Main.java
@@ -0,0 +1,36 @@
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+    EditText nameText,emailText,phoneText;
+    String name,email,phone;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nameText = (EditText) findViewById(R.id.signupnameid);
+        emailText = (EditText)findViewById(R.id.signupemailid);
+        phoneText = (EditText) findViewById(R.id.signupphonenoid);
+
+
+    }
+    public void userReg(View view) {
+        name = nameText.getText().toString();
+        email = emailText.getText().toString();
+        phone = phoneText.getText().toString();
+
+        String method = "register";
+        BackgroundTask backgroundTask = new BackgroundTask(this);
+        backgroundTask.execute(method,email,name,phone);
+        finish();
+
+
+    }
+}
